cd715b8269211675dc7b395ab7808537aed87f2a
Sergey Lobov <loboff09@gmail.com>
2023-01-27 22:55:41 +0300

    fixed

--- utils.py
+++ utils.py
@@ -2,22 +2,17 @@ from collections import Counter
 
 
 def data_processing(command, value, data):
+    data_counter = Counter(data)
     if command == "filter":
         return filter(lambda string_data: value in string_data, data)
     elif command == "map":
         return map(lambda string_data: string_data.split(' ')[int(value) - 1], data)
     elif command == "unique":
-        return
+        return map(lambda string: string[0], data_counter.most_common())
     elif command == "sort":
         if value == "desc":
             return map(lambda x: x, sorted(data, reverse=True))
         else:
             return map(lambda x: x, sorted(data, reverse=False))
     elif command == "limit":
-        data_counter = Counter(data)
         return map(lambda string: string[0], data_counter.most_common(int(value)))
-
-
-
-
-
